ed758545b874954d11a5c31e67ff582231301166
Nivedita Balunavar <120191232+NiveditaB@users.noreply.github.com>
2026-02-17 18:25:39 +0530

    Add C++ data types program

---
+++ datatype.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a = 6; //variable name a whose data type is int has value 6 stored in it.
+    float b = 3.45; //b is the variable name of float data type which stores decimal num.
+    char c = 'n'; // char data type used to store characters in single quotation.
+    bool is_true = true; //bool stores only true or false and prints 1 if true and 0 if false.
+    double d = 8.987376; //double is just like float but will high precision.
+    cout<<"The value of a is: "<<a<<"\n";
+    cout<<"The value of b is: "<<b<<"\n";
+    cout<<"The value of c is: "<<c<<"\n";
+    cout<<"The value of d is: "<<d<<"\n";
+    cout<<"The value of is_true is: "<<is_true<<"\n";
+    return 0;
+
+
+}
